aea3914428bf67f7ec97c60b0cf222ad6d08be2c
Luís Fernando Almeida <luisfernalme@gmail.com>
2023-05-05 12:12:26 -0300

    arquivo main.cpp criado, arquivo makefile criado, alguns comentários adicionados

--- pacman-sin141/map.h
+++ pacman-sin141/map.h
@@ -37,26 +37,27 @@ char map[MAP_LINHAS][MAP_COLUNAS] = {
 };
 
 void draw_map() {
-	Wall wall_obj[200];
-	Coin coin_obj[160];
+	Wall wall_obj[200]; // tamanho do vetor = qntdWall 
+	Coin coin_obj[160]; // tamanho do vetor = qntdCoin
+
 	int linha, coluna, qntdWall= 0 , qntdCoin = 0;
 	for (linha = 0; linha < MAP_LINHAS; linha++) {
 		for (coluna = 0; coluna < MAP_COLUNAS; coluna++) {
 			if (map[linha][coluna] == '0') {
 				wall_obj[qntdWall].draw_wall(linha, coluna);
-				printf("Parede criada em [%d][%d].\n", linha, coluna);
-				qntdWall++;
+				//printf("Parede criada em [%d][%d].\n", linha, coluna);
+				qntdWall++; 
 			}
 			if (map[linha][coluna] == '1') {
 				coin_obj[qntdCoin].draw_coin(linha, coluna);
-				printf("Moeda criada em [%d][%d].\n", linha, coluna);
+				//printf("Moeda criada em [%d][%d].\n", linha, coluna);
 				qntdCoin++;
 
 			}
 		}
 	}
-	cout << "Moedas criadas: " << qntdCoin << endl; //151
-	cout << "Paredes criadas: " << qntdWall << endl; //198
+	cout << "Moedas criadas: " << qntdCoin << endl; //151 moedas no mapa original
+	cout << "Paredes criadas: " << qntdWall << endl; //198 paredes no mapa original
 }
 
 #endif // !MAP_H
